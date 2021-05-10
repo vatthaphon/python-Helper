d612b460a2a901e3c42f3acef2f1df75cbcf0773
Atthaphon Viriyopase <vattha@Atthaphons-MacBook-Pro.local>
2021-05-10 12:40:03 +0700

    update

--- AV_helper.py
+++ AV_helper.py
@@ -854,7 +854,7 @@ def AV_append_to_excel(FN_p, new_rows_p, sheet_name_p='Sheet1'):
 	ws_l = wb_l[sheet_name_p]
 	row_l = ws_l.max_row + 1
 
-	for col, entry in enumerate(new_row_p, start=1):
+	for col, entry in enumerate(new_rows_p, start=1):
 		ws_l.cell(row=row_l, column=col, value=entry)
 
 	wb_l.save(FN_p)
